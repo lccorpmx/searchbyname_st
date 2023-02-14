82c9100846212b1e1e1aa73d504a11de7a90b74e
LCC CORP <lcccorpmx@gmail.com>
2023-02-14 09:57:08 -0600

    push first program

---
+++ secondDB.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pandas as pd
+
+st.title("search name")
+DATA_URL = "dataset.csv"
+
+
+st.cache
+
+def load_data_byname(name):
+	data = pd.read_csv(DATA_URL)
+	filtered_data_byname = data[data["name"].str.contains(name)]
+	return filtered_data_byname
+
+myname = st.text_input("nombre: ")
+
+if (myname):
+	filterbyname = load_data_byname(myname)
+	count_row = filterbyname.shape[0]
+	st.write(f"total names: {count_row}")
+	st.dataframe(filterbyname)
